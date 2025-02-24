5cd430b92f56fb98ce5c9bbc930df0b3e826ceb8
Milica Mandic <mami23qx@student.ju.se>
2025-02-24 15:40:57 +0100

    responsiveness, fixed home page section and hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
+/*chat gpt used for guidance on how to write this code https://chatgpt.com/share/67bc794d-49a8-8003-9426-78d726121a28 along with https://www.reddit.com/r/learnjavascript/comments/me9kz9/checking_the_index_of_element_in_images_array/  */
+
 const heroSection = document.querySelector(".hero");
 const discoverBtn = document.querySelector(".discover-btn");
-const images = ["../img/hero1.jpg", "../img/hero2.jpg", "../img/hero3.jpeg"];
+const images = ["../img/hero2.jpg", "../img/hero1.jpg", "../img/hero3.jpeg"];
 let index = 0;
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navLinks = document.querySelector(".navigation-links");
 
 discoverBtn.addEventListener("mouseenter", () => {
   index = (index + 1) % images.length;
   heroSection.style.backgroundImage = `url(${images[index]})`;
+});
+
+hamburgerMenu.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
 });
